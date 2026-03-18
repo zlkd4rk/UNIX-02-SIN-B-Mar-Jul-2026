f6b070ba33ae067e16af1068da8bd6e8e8b986b9
zlkd4rk <jupesantezto@uide.edu.ec>
2026-03-18 14:00:24 +0000

    new commit

--- practical_workshop_1.sh
+++ practical_workshop_1.sh
@@ -4,3 +4,5 @@ gpg --version
 #empieza con punto empieza es un archivo oculto.
 #RSA 4096, universal y compatible 
 gpg --full-generate-key
+gpg --list-keys
+gpg --armor --export boldstepandrex648@gmail.com > mi_llave_publica.asc
