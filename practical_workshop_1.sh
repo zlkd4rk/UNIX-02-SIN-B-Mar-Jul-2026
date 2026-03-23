9b257f378a8295e6e3c9831c064071a6603cae44
zlkd4rk <jupesantezto@uide.edu.ec>
2026-03-23 12:48:31 +0000

    other commit

--- practical_workshop_1.sh
+++ practical_workshop_1.sh
@@ -8,7 +8,6 @@ gpg --armor --export boldstepandrex648@gmail.com > mi_llave_publica.asc #export 
 ls 
 cat mi_llave_publica.asc
 gpg -list-secret-keys --keyid-format=long  #list about private keys 
-gpg --armor --export-secret-key 9CE2A432BF706712
-#armor 
-gpg -armor -export-secret-keys
-gpg --armor --export boldstepandrex648@gmail.com > mi_llave_publica.asc
+gpg --armor --export-secret-key 9CE2A432BF706712 # we can use it to list our pair of keys and can execute:
+gpg -armor --export-secret-keys # 
+gpg --armor --export boldstepandrex648@gmail.com > mi_llave_publica.asc #repeat the process to create a file with the public key
