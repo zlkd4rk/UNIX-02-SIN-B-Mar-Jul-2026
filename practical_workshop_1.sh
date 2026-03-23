da85c26bb8964cce7d5425d4ed8d0c46b47bcaf5
zlkd4rk <jupesantezto@uide.edu.ec>
2026-03-23 12:31:14 +0000

    New commands

--- practical_workshop_1.sh
+++ practical_workshop_1.sh
@@ -5,4 +5,9 @@ gpg --version
 #RSA 4096, universal y compatible 
 gpg --full-generate-key
 gpg --list-keys
-gpg --armor --export boldstepandrex648@gmail.com > mi_llave_publica.asc
+gpg --armor --export boldstepandrex648@gmail.com > mi_llave_publica.asc #exportar llave publia
+ls 
+cat mi_llave_publica.asc
+gpg -list-secret-keys --keyid-format=long  #listar las llaves privadas 
+gpg --armor --export-secret-key 9CE2A432BF706712
+#armor 
