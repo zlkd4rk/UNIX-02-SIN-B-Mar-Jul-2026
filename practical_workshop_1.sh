8b9899291ab7bcebd2864abde22973650af86924
zlkd4rk <jupesantezto@uide.edu.ec>
2026-03-23 12:33:32 +0000

    new commands 1.1

--- practical_workshop_1.sh
+++ practical_workshop_1.sh
@@ -1,13 +1,12 @@
 uname -a
 which gpg 
 gpg --version
-#empieza con punto empieza es un archivo oculto.
 #RSA 4096, universal y compatible 
 gpg --full-generate-key
 gpg --list-keys
-gpg --armor --export boldstepandrex648@gmail.com > mi_llave_publica.asc #exportar llave publia
+gpg --armor --export boldstepandrex648@gmail.com > mi_llave_publica.asc #export my public keys 
 ls 
 cat mi_llave_publica.asc
-gpg -list-secret-keys --keyid-format=long  #listar las llaves privadas 
+gpg -list-secret-keys --keyid-format=long  #list abour private keys 
 gpg --armor --export-secret-key 9CE2A432BF706712
 #armor 
