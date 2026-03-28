e47d8089f64efa1627a5286fcce55210e62fb7df
zlkd4rk <jupesantezto@uide.edu.ec>
2026-03-28 21:17:41 +0000

    Branch Filesystem correction and pull request commit

--- saludo.c
+++ saludo.c
@@ -1,9 +1,9 @@
-#include <stdio.h> //start the code with library
+#include <stdio.h> //Start the code with a library, this include the Standar Input/Output library.
 //
 int main(){  //the principal function, and return a int number to comprobate the code run correctly 
-    printf("Hello World!\n"); //The message is about to print in the terminal and use \n to jump a line
+    printf("Hello World!\n"); //The message is about to print in the terminal and use \n for a newline.
 
-    return 0; //If return a 0, indicate the code dont hace errors. 
+    return 0; //If return a 0, indicate the code runs correctly with no errors.
 
 
 
