0bca6846abe06169cdc4746d048e591c1c783db5
zlkd4rk <jupesantezto@uide.edu.ec>
2026-03-30 13:12:28 +0000

    UEFI BIOS AND(&&)

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -1,7 +1,8 @@
 sudo apt update # This command synchronizes local package index files with their remote sources. Now the local system has an updated map of the dependency structure and versions available on the server, allowing it to calculate which packages are candidates for an upgrade
-sudo apt upgrade # 
-sudo apt install parted #
+sudo apt upgrade ## Manage updates in the installed packages branch without altering the fundamental system structure.
+sudo apt install parted #This command performs the retrieval, dependency resolution, and installation of a specific package and its necessary shared objects.
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e 
 #&& only is the last command is true execute the next command (AND gate, logic multiplication)
 
-[
+[ -d/sys/firmware/efi ] && echo "EUFI" || echo "BIOS"
+
