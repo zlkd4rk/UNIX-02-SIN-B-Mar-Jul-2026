53ec3d81a83390a22819436f748d5f81ec0371cc
zlkd4rk <jupesantezto@uide.edu.ec>
2026-03-30 13:04:30 +0000

    commands comits

---
+++ fs_exploration.sh
@@ -0,0 +1,7 @@
+sudo apt update # This command synchronizes local package index files with their remote sources. Now the local system has an updated map of the dependency structure and versions available on the server, allowing it to calculate which packages are candidates for an upgrade
+sudo apt upgrade # 
+sudo apt install parted #
+sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e 
+#&& only is the last command is true execute the next command (AND gate, logic multiplication)
+
+[
