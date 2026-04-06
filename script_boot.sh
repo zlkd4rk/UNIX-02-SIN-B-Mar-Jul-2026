8de32a489bfa517bc8dea00b24be468058b94689
zlkd4rk <jupesantezto@uide.edu.ec>
2026-04-06 12:33:43 +0000

    first commands commits

--- script_boot.sh
+++ script_boot.sh
@@ -1,4 +1,4 @@
 cd #change directory
-cd /home/codespace
-cd ~
+cd /home/codespace #It takes you to a specific location starting from the "root" of the system
+cd ~ #Fast access to the home directory.
 cd $HOME #variable valid for the entire operating system
