9b398fe4fe195e6b5f226c8475319ee7d7576905
zlkd4rk <jupesantezto@uide.edu.ec>
2026-04-06 12:52:54 +0000

    stat commit

--- script_boot.sh
+++ script_boot.sh
@@ -10,3 +10,17 @@ ls -lai #list with (l=long) showing the information like permisssions, owner of 
 total 12
 925560 drwxr-xr-x 2 codespace codespace 4096 Apr  6 12:33 .
 918515 drwxr-x--- 1 codespace codespace 4096 Apr  6 12:33 ..
+
+
+stat . #See the metadata of the directory/file
+#512 bytes per block (Posix).
+  File: .
+  Size: 4096            Blocks: 8          IO Block: 4096   directory
+Device: 0,45 # Major and minor, 0 cuz the fylesystem is not a real disc, 45 minimun specific number for taht instance.
+
+    Inode: 925560      Links: 2
+Access: (0755/drwxr-xr-x)  Uid: ( 1000/codespace)   Gid: ( 1000/codespace)
+Access: 2026-04-06 12:34:47.154366038 +0000
+Modify: 2026-04-06 12:33:51.818368401 +0000
+Change: 2026-04-06 12:33:51.818368401 +0000
+ Birth: 2026-04-06 12:33:51.818368401 +0000
