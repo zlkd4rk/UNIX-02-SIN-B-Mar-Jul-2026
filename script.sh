23e5570a67b39c67729a85d2505e64558c511efd
zlkd4rk <jupesantezto@uide.edu.ec>
2026-04-13 12:33:08 +0000

    Structure Commit

---
+++ script.sh
@@ -0,0 +1,10 @@
+Structure of linux command
+command
+subcommand
+options 
+arguments 
+
+
+ls -a #List showing all the files
+ls --all #Same commmand but with the extende option (--all = -a)
+#In Linux we have commands with the short or large option. 
