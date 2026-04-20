3c62fb0eb4d0ab209b28e88b28cb3962beca05e7
zlkd4rk <jupesantezto@uide.edu.ec>
2026-04-20 13:01:07 +0000

    history commit

---
+++ prueba.sh
@@ -0,0 +1,47 @@
+@zlkd4rk ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (eval_p2_1_v1) $ history 
+    1  mkdir nebula/bitacoras
+    2  mv old-dir-name nebula new-dir-name bitacoras
+    3  mv nebula/oldname bitacoras/newname
+    5  mv nebula/nombre_actual nebula/bitacoras
+    6  clear
+    7  mv nebula/nombre_actual nebula/bitacoras
+    8  ls nebula/
+    9  mv nebula/NOMBRE_QUE_VISTE nebula/bitacoras
+   10  mkdir nebula/bitacoras
+   11  mkdir nebula/borradores
+   12  mv cosmos.txt > nebula/bitacoras
+   13  clear
+   14  mv nebula/cosmos.txt nebula/bitacoras/
+   15  mv nebula/estrella.bak nebula/estrella.conf
+   16  man mv
+   17  mv nebula/bitacoras/estrella.conf nebula/bitacoras/estrella.bak
+   18  mv nebula/bitacoras  nebula/registros
+   19  mv nebula/registros  nebula/bitacoras
+   20  clear
+   21  sudo chmod 640 nebula/bitacoras/cosmos.txt
+   22  ls -l nebula/bitacoras/cosmos.txt
+   23  clear
+   24  chmod u+x nebula/galaxia.sh
+   25  sudo chmod u+x nebula/galaxia.sh
+   26  sudo chmod o+r nebula/estrella.conf
+   27  ls -l nebula/galaxia.sh
+   28  ls -l nebula/galaxia.sh nebula/estrella.conf
+   29  sudo chmod o-r nebula/estrella.conf
+   30  ls -l nebula/estrella.conf
+   31  sudo chmod o-w nebula/estrella.conf
+   32  ls -l nebula/galaxia.sh nebula/estrella.conf
+   33  sudo chmod 766 nebula/estrella.conf
+   34  ls -l nebula/galaxia.sh nebula/estrella.conf
+   35  sudo chmod o-w nebula/estrella.conf
+   36  ls -l  nebula/estrella.conf
+   37  chmod u+s nebula/galaxia.sh
+   38  ls -l nebula/galaxia.sh
+   39  ./nebula/galaxia.sh
+   40  ls /tmp/nebula_zone
+   41  mkdir /tmp/nebula_zone
+   42  ls /tmp/nebula_zone
+   43  chmod 1775 tmp/nebula_zone
+   44  chmod +t /tmp/nebula_zone
+   45  ls -ld /tmp/nebula_zone
+   46  history 
+@zlkd4rk ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (eval_p2_1_v1) $ 
