6a15388fec2cd43f607700d17e69d7f0ce48f264
zlkd4rk <jupesantezto@uide.edu.ec>
2026-04-21 21:33:57 +0000

    prueba_correction.sh

--- prueba_correction.sh
+++ prueba_correction.sh
@@ -21,3 +21,43 @@
    21  cd bitacoras
    22  ls -l
    23  history
+   #Second part 
+   @zlkd4rk ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (eval_p1_1_v1_correction) $ history
+    1  ls -l
+    2  cd nebula
+    3  cd bitacoras
+    4  ls -l
+    5  chmod 640 cosmos.txt
+    6  ls -l
+    7  cd ..
+    8  ls
+    9  ls- lah
+   10  ls -lah
+   11  chmod u+x galaxia.sh
+   12  chmod o-r estrella.conf
+   13  ls -lah
+   14  chmod o+r estrella.conf
+   15  chmod o-w estrella.conf
+   16  ls -lah
+   17  ls
+   18  chmod u+s galaxia.sh
+   19  ls -l
+   20  mkdir /tmp/nebula_zone
+   21  ls /tmp
+   22  chmod +t /tmp/nebula_zone
+   23  ls -ld /tmp
+   24  ls -l ]/tmp/nebula_zone
+   25  ls -l /tmp/nebula_zone
+   26  which gpg|gpg --version
+   27  cd nebula
+   28  gpg --full-generate-key
+   29  cd ..
+   30  gpg --recipient aurora --output nebula/bitacoras/cosmos.txt.gpg --encrypt nebula/bitacoras/cosmos.txt
+   31  gpg --list-key
+   32  gpg  aurora --clearsign --output nebula/estrella.conf.asc nebula/estrella.conf
+   33  gpg --clearsign --output nebula/estrella.conf.asc nebula/estrella.conf
+   34  gpg --verify nebula/galaxia.sh.sig nebula/galaxia.sh
+   35  gpg --detach-sign --output nebula/galaxia.sh.sig nebula/galaxia.sh
+   36  gpg --verify nebula/galaxia.sh.sig nebula/galaxia.sh
+   37  History
+   38  history
