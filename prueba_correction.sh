12c442aa764dcda30044214fb6caca314ef91fa8
zlkd4rk <jupesantezto@uide.edu.ec>
2026-04-21 03:54:11 +0000

    first history correction

---
+++ prueba_correction.sh
@@ -0,0 +1,23 @@
+    1  ls
+    2  cd  nebula
+    3  mkdir registros
+    4  mv registros bitacoras
+    5  ls -lah
+    6  mv cosmos.txt nebuela/bitacoras
+    7  mv cosmos.txt nebuela/bitacoras/
+    8  cd ..
+    9  mv cosmos.txt nebuela/bitacoras/
+   10  mv cosmos.txt nebula/bitacoras
+   11  mv cosmos.txt bitacoras/
+   12  lsd
+   13  ls
+   14  ls /bitacoras
+   15  ls bitacoras
+   16  cd nebula
+   17  ls bitacoras
+   18  mv cosmos.txt bitacoras
+   19  ls
+   20  mv estrella.bak estrella.conf
+   21  cd bitacoras
+   22  ls -l
+   23  history
