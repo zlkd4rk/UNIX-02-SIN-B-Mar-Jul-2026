9df2ba585107589f59cef99a7f29388cc02d2449
zlkd4rk <jupesantezto@uide.edu.ec>
2026-05-04 12:46:30 +0000

    See the user id and the users of the system

---
+++ security.sh
@@ -0,0 +1,2 @@
+id # With that you can see the identification of the user(Numeric ID), primary group and also the secundary groups.
+cat /etc/passwd | head -10 #See al the user of the system, but with the pipeline only see the first 10 users.
