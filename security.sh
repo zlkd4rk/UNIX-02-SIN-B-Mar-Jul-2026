2a2108438f46e6ef5907edc7b402c071aa1da181
zlkd4rk <jupesantezto@uide.edu.ec>
2026-05-06 13:36:34 +0000

    Change the gruop of a file

--- security.sh
+++ security.sh
@@ -132,3 +132,11 @@ total 8
 drwxr-xr-x 2 root root 4096 may  6 13:30 .
 drwxr-xr-x 5 root root 4096 may  6 13:27 ..
 -rw-r--r-- 1 root root    0 may  6 13:30 informe.txt
+
+chgrp desarrolladores ~/lab_chgrp/proyectos/app.py
+s -la ~/lab_chgrp/proyectos/
+total 8
+drwxr-xr-x 2 root root            4096 may  6 13:29 .
+drwxr-xr-x 5 root root            4096 may  6 13:27 ..
+-rw-r--r-- 1 root desarrolladores    0 may  6 13:28 app.py
+-rw-r--r-- 1 root root               0 may  6 13:29 config.json
