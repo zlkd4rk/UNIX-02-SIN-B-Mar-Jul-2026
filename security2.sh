6485731bf83a836b104e4045d41c93f791347c07
zlkd4rk <jupesantezto@uide.edu.ec>
2026-05-27 13:23:36 +0000

    Commit 3

--- security2.sh
+++ security2.sh
@@ -10,3 +10,24 @@ echo "Grupo actual: $(id -gn)"
 #Create a file before the use of newgrp
 touch ~/antes_de_newgrp.txt
 ls -la ~/antes_de_newgrp.txt
+
+
+newrgp desarrolladores
+#@zlkd4rk ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (security2) $ newgrp desarrolladores
+#newgrp: group 'desarrolladores' does not exist
+
+id -gn
+echo "Nuevo grupo activo $(id -gn)"
+#@zlkd4rk ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (security2) $ echo "Nuevo grupo activo $(id -gn)"
+#Nuevo grupo activo desarrolladores
+
+#/workspaces/UNIX-02-SIN-B-Mar-Jul-2026 # groupadd desarrolladores
+#/workspaces/UNIX-02-SIN-B-Mar-Jul-2026 # exit
+#@zlkd4rk ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (security2) $ id -gn
+#vscode
+#@zlkd4rk ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (security2) $ echo "Nuevo grupo activo $(id -gn)"
+#Nuevo grupo activo vscode
+#@zlkd4rk ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (security2) $ sudo adduser vscode desarrolladores
+#@zlkd4rk ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (security2) $ newgrp desarrolladores
+#@zlkd4rk ➜ /workspaces/UNIX-02-SIN-B-Mar-Jul-2026 (security2) $ echo "Nuevo grupo activo $(id -gn)"
+#Nuevo grupo activo desarrolladores
