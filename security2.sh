899d0701fabc29172de2879cbd6aa3c28e6aa214
zlkd4rk <jupesantezto@uide.edu.ec>
2026-05-27 13:06:50 +0000

    Commit 2

--- security2.sh
+++ security2.sh
@@ -4,3 +4,9 @@ id -gn #Only the name of the principal group
 touch ~/test_grupo_heredado.txt
 ls -la ~/test_grupo_heredado.txt
 #the group is the main group of the user.
+
+#See the current group
+echo "Grupo actual: $(id -gn)"
+#Create a file before the use of newgrp
+touch ~/antes_de_newgrp.txt
+ls -la ~/antes_de_newgrp.txt
