6e0899ef6f4abb4f0c3a78e6fa51d36e0f05b6e1
zlkd4rk <jupesantezto@uide.edu.ec>
2026-06-01 13:05:29 +0000

    Restricted bash

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,3 +1,21 @@
+#!/bin/bash -x  
+#This option prints all commands and their arguments as they are executed to the terminal.
+
 bash --version
 env 
+
 echo ${SHELL}
+echo ${RANDOM}
+echo ${UID}
+echo ${OSTYPE}
+
+ps -ef
+df --human-readable #df -h
+
+chmod 777 blackhatbash1.sh
+./blackhatbash1.sh
+bash -r blackhatbash1.sh #This option creates a restricted bash shell, which restricts certain potentially dangerous commands.
+
+
+
+
