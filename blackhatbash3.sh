bee98d65b09c66d6ca5488dff5e12dce5f67ac87
zlkd4rk <jupesantezto@uide.edu.ec>
2026-06-01 13:43:10 +0000

    unassigning Variables

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -3,3 +3,11 @@ book="Black hat bash"
 echo "This book´s name is ${book}"
 
 echo "This book´s name is $book"
+
+root_directory=$(ls -ld /)
+echo "${root_directory}"
+
+
+book="Black Hat Bash"
+unset book
+echo "${book}"
