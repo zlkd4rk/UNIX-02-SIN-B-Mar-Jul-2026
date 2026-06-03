05b73149470f1065fc5c92b5a2503bc43934880e
zlkd4rk <jupesantezto@uide.edu.ec>
2026-06-03 12:46:13 +0000

    Unset a varible in an array an print all te elements

--- array.sh
+++ array.sh
@@ -5,3 +5,5 @@ IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
 echo "${IP_ADDRESSES[*]}"
 # Prints only the first element in the array
 echo "${IP_ADDRESSES[0]}"
+unset IP_ADDRESSES[1]
+echo "${IP_ADDRESSES[*]}"
