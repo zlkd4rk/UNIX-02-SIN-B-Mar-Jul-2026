42f9a46e49c456d4e08520b76ad8c1cf9b464d37
zlkd4rk <jupesantezto@uide.edu.ec>
2026-06-08 14:02:18 +0000

    Comments

--- exercise1.sh
+++ exercise1.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-FIRST_NAME="${1}"
+FIRST_NAME="${1}" #First Argument
 
-LAST_NAME="${2}"
+LAST_NAME="${2}" #Second Argument
 
-touch output.txt
+touch output.txt #Create the file to save the output of the commands
 
-date +%M-%d-%Y >> output.txt
+date +%M-%d-%Y >> output.txt  #Save the date into a MM/DD/YY format
 
-echo "${FIRST_NAME} ${LAST_NAME}" >> output.txt
+echo "${FIRST_NAME} ${LAST_NAME}" >> output.txt #Echo to the arguments ans save it into output.txt
 
-cp output.txt backup.txt
+cp output.txt backup.txt #Copy the file and create a new one called backup.txt
 
-cat output.txt
+cat output.txt #At the end cat the file and see the content
+#Also you can cat the backup.txt file to see the same result
+
 
