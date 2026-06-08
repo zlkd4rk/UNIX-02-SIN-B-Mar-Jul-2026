ca0a0175c6110dfcffa201d30416a3486f024aa7
zlkd4rk <jupesantezto@uide.edu.ec>
2026-06-08 12:42:00 +0000

    Script with arguments

---
+++ ping_with_arguments.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+# This script will ping any address provided as an argument.
+SCRIPT_NAME="${0}"
+TARGET="${1}"
+echo "Running the script ${SCRIPT_NAME}..."
+echo "Pinging the target: ${TARGET}..."
+ping "${TARGET}"
