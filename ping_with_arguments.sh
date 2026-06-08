855af1499174af5906b565d2fd07d21fd6b26caf
zlkd4rk <jupesantezto@uide.edu.ec>
2026-06-08 12:57:36 +0000

    Extende the script with for bucle for each argument

--- ping_with_arguments.sh
+++ ping_with_arguments.sh
@@ -8,3 +8,7 @@ ping -c 3 "${TARGET}" #Only do 3 pings.
 
 echo "The arguments are: $@"
 echo "The total number of arguments is: $#"
+# Change "$@" to "$*" to observe behavior.
+for args in "$@"; do
+echo "${args}"
+done
