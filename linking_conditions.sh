4a4a9dc71aa988144061fac392ba393bdd5be961
zlkd4rk <jupesantezto@uide.edu.ec>
2026-06-10 12:48:29 +0000

    Onother linking conditions

--- linking_conditions.sh
+++ linking_conditions.sh
@@ -3,3 +3,9 @@ echo "Hello World!" > file.txt
 if [[ -f "file.txt" ]] && [[ -s "file.txt" ]]; then
 echo "The file exists and its size is greater than zero."
 fi
+
+DIR_NAME="dir_test"
+mkdir "${DIR_NAME}"
+if [[ -f "${DIR_NAME} " ]] || [[ -d "${DIR_NAME}" ]]; then
+echo "${DIR_NAME} is either a file or a directory."
+fi
