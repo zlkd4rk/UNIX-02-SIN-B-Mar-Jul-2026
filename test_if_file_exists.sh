6590752d6fc4def2f3d2d6a14af8e27dd83e8326
zlkd4rk <jupesantezto@uide.edu.ec>
2026-06-10 12:28:39 +0000

    if conditions in less lines

--- test_if_file_exists.sh
+++ test_if_file_exists.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-FILENAME="flow_control_with_if.txt"
-if [[ -f "${FILENAME}" ]]; then
-echo "${FILENAME} already exists."
-exit 1
-else
+FILENAME="flow_control_with_if1.txt"
+if [[ ! -f "${FILENAME}" ]]; then
 touch "${FILENAME}"
 fi
