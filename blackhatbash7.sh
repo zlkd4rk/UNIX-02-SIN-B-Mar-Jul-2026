e7c46c4da01f0e72080b4fd606bce6dff9bb8d55
zlkd4rk <jupesantezto@uide.edu.ec>
2026-06-15 13:12:14 +0000

    filtering with grep

---
+++ blackhatbash7.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+grep "35.237.4.214" log.txt
+grep "35.237.4.214\|13.66.139.0" log.txt
+grep -e "35.237.4.214" -e "13.66.139.0" log.txt
+ps | grep TTY
+ps | grep -i tty
+grep -v "35.237.4.214" log.txt
+grep -o "35.237.4.214" log.txt
