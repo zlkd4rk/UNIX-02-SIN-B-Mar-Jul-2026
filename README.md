4fb8e4a52630cff65764d71b319f12432478cec5
zlkd4rk <jupesantezto@uide.edu.ec>
2026-06-15 12:26:53 +0000

    until loop commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # UNIX-02-SIN-B-Mar-Jul-2026
 Repo for the subject intoduction to unix
+![alt text](image.png)
---
+++ until_loop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FILE="output.txt"
+touch "${FILE}"
+until [[ -s "${FILE}" ]]; do
+echo "${FILE} is empty..."
+echo "Checking again in 2 seconds..."
+sleep 2
+done
+echo "${FILE} appears to have some content in it!"
