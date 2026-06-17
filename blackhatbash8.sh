fcb2d0f39573dc7c07c999938e15fefb84726926
zlkd4rk <jupesantezto@uide.edu.ec>
2026-06-17 13:12:24 +0000

    Sed commits to delete some lines in a specific position

--- blackhatbash8.sh
+++ blackhatbash8.sh
@@ -12,3 +12,14 @@ awk -F',' '{print $1}' example_csv.txt
 awk 'NR < 10' log.txt
 
 grep "42.236.10.117" log.txt | awk '{print $7}'
+
+sed 's/ //g' log.txt
+sed 's/Mozilla/Godzilla/g' log.txt > newlog.txt
+grep "Godzilla" newlog.txt
+
+
+sed '1d' log.txt >> newlogd.txt
+sed '$d' log.txt >> newlogl.txt
+sed '5,7d' log.txt >> newlog57.txt
+sed -n '2,15 p' log.txt
+sed -i '1d' log.txt
